eb6d0145392bf92975b098fa72d213aab1647709
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-10-12 00:47:21 +0500

    calculator done

---
+++ app.js
@@ -0,0 +1,24 @@
+var characterDisplay = "";
+var oprStore = "";
+
+function number(num){
+    characterDisplay += num;
+    document.getElementById("result").value = characterDisplay; 
+}
+
+function opr(opr){
+    if(isNaN(characterDisplay.slice(-1))){
+        characterDisplay = characterDisplay.slice(0, -1)
+        
+    }
+    characterDisplay += opr;
+    document.getElementById("result").value = characterDisplay; 
+}
+
+function outputCalcu() {
+    if (characterDisplay !== "") {
+        var result =  eval(characterDisplay)
+        document.getElementById("result").value = result; 
+    }
+}
+document.getElementById("result").value = ""; 

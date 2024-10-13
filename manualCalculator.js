fe27d3ec9a3ff76387f86420e64f235e785c9234
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-10-13 23:58:06 +0500

    added new logic

--- manualCalculator.js
+++ manualCalculator.js
@@ -1,57 +1,51 @@
 var characterDisplay = "";
 var oprStore = "";
 
-function number(num){
+function number(num) {
     characterDisplay += num;
     document.getElementById("result").value = characterDisplay; 
 }
 
-function opr(opr){
-    if(!isNaN(characterDisplay[0])){
-
-    if(isNaN(characterDisplay.slice(-2))){
-        characterDisplay = characterDisplay.slice(0, -2)
+function opr(opr) {
+    if (!isNaN(characterDisplay[0])) {
+        if (isNaN(characterDisplay.slice(-1))) {
+            characterDisplay = characterDisplay.slice(0, -1); // Remove last operator if present
+        }
+        characterDisplay += " " + opr + " "; // Add spaces around operator
+        oprStore += opr; // Store operator
+        document.getElementById("result").value = characterDisplay; 
+    } else {
+        alert('first enter any numeric value');
     }
-    characterDisplay += " " + opr + " ";
-    document.getElementById("result").value = characterDisplay; 
-}else{
-    alert('first enter any numaric value')
-}
 }
 
 function outputCalcu() {
-    charArray = characterDisplay.split(" ")
+    var charArray = characterDisplay.split(" ");
+    var resultDisplay = ""
     
     for(var i = 0; i < charArray.length; i++){
-        var opr = charArray[i];
-        var beforeChar = +charArray[i-1];
-        var afterChar = +charArray[i+1] 
+            if(charArray[i] === '/'){
+                resultDisplay = (charArray[i-1] / charArray[i+1]).toFixed()
+                charArray.splice(i-1, 3,resultDisplay.toString())
 
-        switch(opr){
-                case "/":
-                var result  = (beforeChar/afterChar).toString();
-                charArray.splice(i-1,3);
-                charArray.splice(i-1,0,result)
-                break
-                
-                case "*":
-                    var result  = (beforeChar*afterChar).toString();
-                    charArray.splice(i-1,3);
-                    charArray.splice(i-1,0,result)
-                    break
-
-                }
             }
-            console.log(charArray);
+    }
+    
+    for(var i = 0; i < charArray.length; i++){
+        if(charArray[i] === '*'){
+            resultDisplay = (charArray[i-1]*charArray[i+1]).toFixed()
+            charArray.splice(i-1, 3,resultDisplay.toString())
+    }
+}
+    console.log('this is final result:', charArray);
 }
 
-
-function clearSingleChar(){
-    characterDisplay = characterDisplay.slice(0, -1)
-document.getElementById("result").value = characterDisplay; 
+function clearSingleChar() {
+    characterDisplay = characterDisplay.slice(0, -1);
+    document.getElementById("result").value = characterDisplay; 
 }
 
-function clearHoleCal(){
-     characterDisplay = "";
-document.getElementById("result").value = characterDisplay; 
+function clearWholeCal() {
+    characterDisplay = "";
+    document.getElementById("result").value = characterDisplay; 
 }
